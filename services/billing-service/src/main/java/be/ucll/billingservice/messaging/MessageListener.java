228a82e14100baaa7e43304e52ae0958f9bbdbed
Ninoverstraeten2003 <r0898606@ucll.be>
2023-11-23 16:11:29 +0100

    compiling issue temp fix

--- services/billing-service/src/main/java/be/ucll/billingservice/messaging/MessageListener.java
+++ services/billing-service/src/main/java/be/ucll/billingservice/messaging/MessageListener.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import jakarta.transaction.Transactional;
 
 @Component
 @Transactional
--- services/reservation-service/src/main/java/be/ucll/reservationservice/messaging/MessageListener.java
+++ services/reservation-service/src/main/java/be/ucll/reservationservice/messaging/MessageListener.java
@@ -1,11 +1,12 @@
 package be.ucll.reservationservice.messaging;
 import be.ucll.reservationservice.domain.ReservationService;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Component
 @Transactional
