6e674a6160dfe242500f5a5a976378d0ff4a1558
Ninoverstraeten2003 <r0898606@ucll.be>
2023-11-26 02:27:04 +0100

    creating the events

--- services/reservation-service/src/main/java/be/ucll/reservationservice/domain/ReservationRequestSaga.java
+++ services/reservation-service/src/main/java/be/ucll/reservationservice/domain/ReservationRequestSaga.java
@@ -1,6 +1,9 @@
 package be.ucll.reservationservice.domain;
-
+import be.ucll.billingservice.api.model.BilledUserEvent;
+import be.ucll.carservice.api.model.ReservedCarEvent;
+import be.ucll.reservationservice.api.model.ConfirmedReservationEvent;
 import be.ucll.reservationservice.messaging.RabbitMqMessageSender;
+import be.ucll.userservice.api.model.NotifiedUserEvent;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,24 +19,24 @@ public class ReservationRequestSaga {
         reservation.reservingCar();
         eventSender.sendReservingCarCommand(reservation.getId(), reservation.getCarId(), reservation.getStartDate(), reservation.getEndDate());
     }
-    /*
+
     public void executeSaga(Integer id, ReservedCarEvent event) {
         Reservation reservation = repository.findById(id).orElseThrow();
-        if(event.carNotListed()) {
+        if(event.getCarNotListed()) {
             reservation.carNotListed();
             System.out.println("Car not listed");
         }
-        if(event.isDoubleBooking){
+        if(event.getIsDoubleBooking()){
             reservation.doubleBooking();
             System.out.println("Double booking");
         }
-        if(!event.carNotListed() && !event.isDoubleBooking){
+        if(!event.getCarNotListed() && !event.getIsDoubleBooking()){
             eventSender.sendConfirmingReservationCommand(reservation.getId());
         }
     }
-    public void executeSaga(Integer id, ComfirmedReservationEvent event) {
+    public void executeSaga(Integer id, ConfirmedReservationEvent event) {
         Reservation reservation = repository.findById(id).orElseThrow();
-        if(event.ownerDeclines()) {
+        if(event.getOwnerDeclines()) {
             reservation.ownerDeclines();
             System.out.println("Owner declines");
         } else {
@@ -42,7 +45,7 @@ public class ReservationRequestSaga {
     }
     public void executeSaga(Integer id, BilledUserEvent event) {
         Reservation reservation = repository.findById(id).orElseThrow();
-        if(event.billingUserFailed()) {
+        if(event.getBillingUserFailed()) {
             reservation.billingUserFailed();
             System.out.println("Billing user failed");
         } else {
@@ -53,7 +56,7 @@ public class ReservationRequestSaga {
     }
     public void executeSaga(Integer id, NotifiedUserEvent event) {
         Reservation reservation = repository.findById(id).orElseThrow();
-        if(event.notifyingUserFailed()) {
+        if(event.getNotifyingUserFailed()) {
             reservation.notifyingUserFailed();
             eventSender.sendReverseBillingCommand(reservation.getId());
             System.out.println("Notifying user failed");
@@ -61,5 +64,5 @@ public class ReservationRequestSaga {
             reservation.userNotified();
             eventSender.sendFinalisingReservationCommand(reservation.getId());
         }
-    }*/
+    }
 }
--- services/reservation-service/src/main/java/be/ucll/reservationservice/messaging/RabbitMqMessageSender.java
+++ services/reservation-service/src/main/java/be/ucll/reservationservice/messaging/RabbitMqMessageSender.java
@@ -1,12 +1,14 @@
 package be.ucll.reservationservice.messaging;
 
+import be.ucll.billingservice.api.model.BillCommand;
+import be.ucll.billingservice.api.model.ReverseBillingCommand;
+import be.ucll.userservice.api.model.NotifyingUserCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import be.ucll.reservationservice.api.model.ReservationCommand;
-import java.time.OffsetDateTime;
+// import be.ucll.reservationservice.api.model.ReservationCommand;
 
 @Component
 public class RabbitMqMessageSender {
@@ -21,7 +23,7 @@ public class RabbitMqMessageSender {
         LOGGER.info("Sending message: " + message);
         this.rabbitTemplate.convertAndSend(queue, message);
     }
-
+    /*
     public void sendReservingCarCommand(Integer userId, Integer carId, OffsetDateTime startTime, OffsetDateTime endTime) {
         var command = new ReservationCommand();
         command.userId(userId);
@@ -29,6 +31,30 @@ public class RabbitMqMessageSender {
         command.startTime(startTime);
         command.endTime(endTime);
         sendToQueue("q.reservation-service.reserving-car", command);
+    }*/
+    /*
+    public void sendConfirmingReservationCommand(Integer id) {
+        var command = new ConfirmingReservationCommand();
+        sendToQueue("q.reservation-service.confirming-reservation", command);
+    }*/
+
+    public void sendBillingUserCommand(Integer id) {
+        var command = new BillCommand();
+        sendToQueue("q.reservation-service.billing-user", command);
     }
 
+    public void sendNotifyingUserCommand(Integer id) {
+        var command = new NotifyingUserCommand();
+        sendToQueue("q.reservation-service.notifying-user", command);
+    }
+
+    public void sendReverseBillingCommand(Integer id) {
+        var command = new ReverseBillingCommand();
+        sendToQueue("q.reservation-service.reverse-billing", command);
+    }
+/*
+    public void sendFinalisingReservationCommand(Integer id) {
+        var command = new FinalisingReservationCommand();
+        sendToQueue("q.reservation-service.finalising-reservation", command);
+    }*/
 }
