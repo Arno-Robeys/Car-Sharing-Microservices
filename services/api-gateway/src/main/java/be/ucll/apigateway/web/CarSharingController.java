9520581fc9249a7ca52f6926886c0094d9ffa7bc
Arno Robeys <arno.robeys@student.ucll.be>
2023-11-26 23:53:05 +0100

    Test API-Gateway

--- services/api-gateway/src/main/java/be/ucll/apigateway/web/CarSharingController.java
+++ services/api-gateway/src/main/java/be/ucll/apigateway/web/CarSharingController.java
@@ -1,7 +1,17 @@
 package be.ucll.apigateway.web;
 
+import be.ucll.apigateway.api.TestApiDelegate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CarSharingController {
+public class CarSharingController implements TestApiDelegate {
+
+    @Override
+    public ResponseEntity<Void> test()
+    {
+        //Met deze methode kan je testen of de api-gateway werkt
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
 }
